a497ad2266efe9f3c555aa4dbbf20c045d83ee6e
gynamics <dybfysiat@163.com>
2025-05-07 01:32:59 +0800

    v5.6.0: OP.28, No.6
    Optimize elisp code for system path management.
    
    Introduce hook decorator framework for hook management.
    
    Replace el-get with vc-use-package.
    
    Remove doom-themes and doom-modeline, we have koishi-theme and
    violet-line currently.
    
    Remove emms, which is useless for me currently.
    
    Add package liquidmetal, company-fuzzy.
    
    Add a patch for `org--make-preview-overlay', see Emacs bug#77104
    
    Fix several small bugs in configuration.
    
    Modify some keybindings for better experience.

--- init.el
+++ init.el
@@ -33,6 +33,13 @@
 (setq gc-cons-threshold (expt 2 26)
       gc-cons-percentage 0.15)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-quickstart t))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
