403645322951612b8bd9799f61ce3749a17fa182
gynamics <dybfysiat@163.com>
2025-11-06 00:38:12 +0800

    5.10.0: Heart Scenery
    Add a `custom-set-faces' section in init.el to avoid it being
    appended after `(provide 'init)' automatically.
    
    Use FreeMono as `fixed-pitch-serif' font.
    
    Enable `show-paren-mode' in `prog-mode'.
    
    New color palette for `highlight-parentheses'.
    
    Adjust document structure with several code optimizations.
    
    Remove package `edit-at-point', `liquidmetal', `company-fuzzy',
    `company-web', `xref-union' and `doc-toc'.
    
    Bugfix and enhancement in `company-mode' configuration.
    
    Add tex configuration.
    
    Not longer activate python virtualenv automatically.
    
    Currently mhtml support is broken, so we use `web-mode' for html.
    Emacs 31 will provide `mhtml-ts-mode', let's wait and see.

--- init.el
+++ init.el
@@ -40,6 +40,12 @@
  ;; If there is more than one, they won't work right.
  '(package-quickstart t))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (let ((literate-config (concat user-emacs-directory "koishimacs.org"))
       (code-config (concat user-emacs-directory "koishimacs.el")))
